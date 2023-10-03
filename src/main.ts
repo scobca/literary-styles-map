b403682ceb6018e6b62633e1e6b37dd5df5a1616
scobca <scobca18@yandex.ru>
2023-10-04 02:43:45 +0300

    importing Chart.js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+//creating App
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+//Chart.js
+import Chart from 'chart.js/auto';
 
 createApp(App).use(store).use(router).mount('#app')
