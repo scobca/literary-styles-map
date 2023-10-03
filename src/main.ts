05dd967251d92af9cbab4a114ae2edcc634117e5
scobca <scobca18@yandex.ru>
2023-10-04 02:59:03 +0300

    deleting ild files, reformat code in App, creating routes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,4 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-//Chart.js
-import Chart from 'chart.js/auto';
-
 createApp(App).use(store).use(router).mount('#app')
