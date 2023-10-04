c1ee71ad7907acb06ffc3604f94331c62dc3d145
scobca <scobca18@yandex.ru>
2023-10-04 03:03:17 +0300

    importing stylesheets, small fixes

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//import stylesheets
+import '@/assets/styles/main.less'
+
 createApp(App).use(store).use(router).mount('#app')
