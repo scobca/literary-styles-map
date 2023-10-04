cd30ae33d10e04eb576720042780d8daf3bfe881
scobca <scobca18@yandex.ru>
2023-10-05 01:55:26 +0300

    adding france styles

--- src/components/YFranceMap.vue
+++ src/components/YFranceMap.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="france_map">
+  <div class="main_container">
     <h2 class="country_header">Франция</h2>
-    <img src="@/assets/img/maps/france_map_fst.png" alt="" class="fm-f img">
-    <img src="@/assets/img/maps/france_map_snd.png" alt="" class="fm-s img">
+    <div class="france_map">
+      <div class="block fm-f_block">
+        <span class="style_name">Реализм</span>
+        <img src="@/assets/img/maps/france_map_fst.png" alt="" class="fm-f img">
+      </div>
+      <div class="block block_reverse fm-s_block">
+        <span class="style_name">Символизм</span>
+        <img src="@/assets/img/maps/france_map_snd.png" alt="" class="fm-s img">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +26,28 @@ export default class YFranceMap extends Vue {
 </script>
 
 <style scoped lang="less">
+.france_map {
+  display: flex;
+  flex-direction: row;
+}
+
+.block {
+  display: flex;
+  flex-direction: row;
+}
+
+.block_reverse {
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.style_name {
+  margin: 1rem;
+  font-weight: bold;
+  scale: 100%;
+  transition-duration: .3s;
+}
+
 .country_header {
   margin-bottom: .75rem;
   margin-left: 1rem;
@@ -25,17 +55,20 @@ export default class YFranceMap extends Vue {
 
 .fm-f {
   height: 10rem;
+  margin-left: -2rem;
   scale: 100%;
   transition-duration: .3s;
 }
 
 .fm-s {
   height: 9.1rem;
-  margin-left: -4.2rem;
-  margin-bottom: -.45rem;
+  margin-left: -4.25rem;
+  margin-top: 1.2rem;
+  margin-right: -2rem;
 }
 
-.fm-f:hover, .fm-s:hover {
+.fm-f_block:hover .style_name, .fm-f_block:hover .fm-f,
+.fm-s_block:hover .fm-s, .fm-s_block:hover .style_name {
   scale: 120%;
   transition-duration: .3s;
   z-index: 100;
