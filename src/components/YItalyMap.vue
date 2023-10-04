637ed191bddc94ba466163ada25cc84c7fcfa08f
scobca <scobca18@yandex.ru>
2023-10-05 02:12:07 +0300

    adding styles for italy

--- src/components/YItalyMap.vue
+++ src/components/YItalyMap.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="italy_map">
+  <div class="main_container">
     <h2 class="country_header">Италия</h2>
-    <img src="@/assets/img/maps/italy_map.png" alt="" class="im-f img">
+    <div class="italy_map">
+      <div class="block im-s-block">
+        <span class="style_name">Футуризм</span>
+        <img src="@/assets/img/maps/italy_map.png" alt="" class="im-f img">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,17 +22,35 @@ export default class YItalyMap extends Vue{
 </script>
 
 <style scoped lang="less">
+.italy_map {
+  display: flex;
+  flex-direction: row;
+}
+
+.block {
+  display: flex;
+  flex-direction: row;
+}
+
 .country_header {
   margin-left: 1rem;
+}
+
+.style_name {
+  margin: 2rem 0;
+  font-weight: bold;
+  scale: 100%;
+  transition-duration: .3s;
 }
 
 .im-f {
   height: 13rem;
   scale: 100%;
   transition-duration: .3s;
+  margin-left: -4rem;
 }
 
-.im-f:hover {
+.im-s-block:hover .im-f, .im-s-block:hover .style_name{
   scale: 120%;
   transition-duration: .3s;
   z-index: 100;
