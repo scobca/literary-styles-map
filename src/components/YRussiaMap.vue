478860145068ba9d7c15f709f189644ed96458b3
scobca <scobca18@yandex.ru>
2023-10-05 02:06:57 +0300

    adding styles for russia

--- src/components/YRussiaMap.vue
+++ src/components/YRussiaMap.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="russia_map">
+  <div class="main_container">
     <h2 class="country_header">Россия</h2>
-    <img src="@/assets/img/maps/russia_map_fst.png" alt="" class="rm-f img">
-    <img src="@/assets/img/maps/russia_map_snd.png" alt="" class="rm-s img">
+    <div class="russia_map">
+      <div class="block rm-f-block">
+        <span class="style_name">Акмеизм</span>
+        <img src="@/assets/img/maps/russia_map_fst.png" alt="" class="rm-f img">
+      </div>
+      <div class="block block_reverse rm-s-block">
+        <span class="style_name">Реализм</span>
+        <img src="@/assets/img/maps/russia_map_snd.png" alt="" class="rm-s img">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +26,28 @@ export default class YRussiaMap extends Vue {
 </script>
 
 <style scoped lang="less">
+.russia_map {
+  display: flex;
+  flex-direction: row;
+}
+
+.block {
+  display: flex;
+  flex-direction: row;
+}
+
+.block_reverse {
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.style_name {
+  margin: 1rem;
+  font-weight: bold;
+  scale: 100%;
+  transition-duration: .3s;
+}
+
 .country_header {
   margin-bottom: .75rem;
   margin-left: 1rem;
@@ -25,6 +55,7 @@ export default class YRussiaMap extends Vue {
 
 .rm-f {
   height: 10rem;
+  margin-left: -3.5rem;
   scale: 100%;
   transition-duration: .3s;
 }
@@ -32,10 +63,11 @@ export default class YRussiaMap extends Vue {
 .rm-s {
   height: 12rem;
   margin-left: -3.6rem;
-  margin-bottom: .15rem;
+  margin-top: -2.2rem;
+  margin-right: -2rem;
 }
 
-.rm-f:hover, .rm-s:hover {
+.rm-f-block:hover .rm-f, .rm-f-block:hover .style_name, .rm-s-block:hover .rm-s, .rm-s-block:hover .style_name {
   scale: 120%;
   transition-duration: .3s;
   z-index: 100;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,11 +25,11 @@ export default class HomeView extends Vue {
 <style scoped lang="less">
 .home_container {
   height: 81vh;
-  width: 70vw;
+  width: 80vw;
 
   margin: 2rem 0 0 0;
   background: url("@/assets/img/map_background_2.png") fixed center no-repeat;
-  background-size: 52rem 35rem;
+  background-size: 62rem 35rem;
 
   display: flex;
   flex-direction: column;
@@ -38,7 +38,7 @@ export default class HomeView extends Vue {
 }
 
 .country_container {
-  max-width: 50vw;
+  max-width: 60vw;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
