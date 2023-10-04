548489272c03e0fbf42d00e462809e8c7356a492
scobca <scobca18@yandex.ru>
2023-10-04 03:11:40 +0300

    creating, importing color less, refactor code to color variables

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 
 //import stylesheets
+import '@/assets/styles/color.less'
 import '@/assets/styles/main.less'
 
 createApp(App).use(store).use(router).mount('#app')
