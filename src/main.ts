31ab6a604df87e7c10c741da7ad064fad939919c
scobca <scobca18@yandex.ru>
2023-10-05 03:20:18 +0300

    importing vuetify

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,13 @@ import '@/assets/styles/color.less'
 import '@/assets/styles/main.less'
 import '@/assets/styles/scrollbar_styles.less'
 
-createApp(App).use(store).use(router).mount('#app')
+//import Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+
+const vuetify = createVuetify({
+  components,
+})
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app')
